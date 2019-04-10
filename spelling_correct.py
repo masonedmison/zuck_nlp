c4254b3a645ce041e40b79e60d8551e4b6953842
Mason Edmison <mzbaran@uwm.edu>
2019-04-10 13:20:18 -0500

    spelling_correct added

---
+++ spelling_correct.py
@@ -0,0 +1,56 @@
+import re
+import collections
+
+
+def tokens(text):
+    """
+    get all words from corpus
+    :param text:
+    :return:
+    """
+    return re.findall('[a-z]+', text.lower())
+
+
+def edits0(word):
+    """
+    returns all strings that are zero edits away - or the word itself
+    :param word:
+    :return:
+    """
+    return{word}
+
+
+def edits1(word):
+    """
+    return all strings that are one edit away
+    :param word:
+    :return:
+    """
+
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
+    def splits(word):
+        """
+        return a list of all possible (first, rest) pairs
+        that the input word is made from
+        :param word:
+        :return:
+        """
+        return[(word[:i], word[i:])
+               for i in range(len(word)+1)]
+    pairs = splits(word)
+    deletes = [a + b[1:] for (a, b) in pairs if b]
+    transposes = [a + b[1] + b[0] + b[2:] for (a,b) in pairs if len(b) > 1]
+    replaces = [a + b + c[1:] for(a,b) in pairs for c in alphabet
+                if b]
+    inserts = [a + c + b for(a, b) in pairs for c in alphabet]
+    return set(deletes + transposes + replaces + inserts)
+
+
+def edits2(word):
+    """
+    return all strings that are two edits away from the input word
+    :param word:
+    :return:
+    """
+    return {e2 for e1 in edits1(word) for e2 in edits1(e1)}
