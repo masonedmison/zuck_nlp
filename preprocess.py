ce28bcd8c86575ba58634448c6e8dbfc41d02ee3
Mason Edmison <mzbaran@uwm.edu>
2019-04-10 12:14:20 -0500

    More preprocessing
    expand_contractions(sent, contraction_map)

--- preprocess.py
+++ preprocess.py
@@ -1,9 +1,15 @@
 import nltk
 import re
 import string
+from contractions import CONTRACTION_MAP
 
 # for testing purposes
-file = '/Users/MasonBaran/Desktop/xml_read_test/2019-006.xml'
+#file = '/Users/MasonBaran/Desktop/xml_read_test/2019-006.xml'
+
+# short test corpus
+corpus = "The brown fox wasn't that quick and he couldn't win the race \
+          Hey that's a great deal! I just bought a phone for $199\
+          @@You'll (learn) a **lot** in the book. Python is an amazing language!@@"
 
 
 def tokenize_text(text):
@@ -28,3 +34,31 @@ def remove_characters_after_tokenization(tokens):
     filtered_tokens = filter(None,[pattern.sub('', token) for token in tokens])
     return filtered_tokens
 
+
+def expand_contractions(sentence, contraction_mapping):
+    contractions_pattern = re.compile('({})'.format('|'.join(contraction_mapping.keys())),
+                                      flags=re.DOTALL|re.IGNORECASE)
+
+    def expand_match(contraction):
+        match = contraction.group(0)
+        first_char = match[0]
+        expanded_contraction = contraction_mapping.get(match)\
+            if contraction_mapping.get(match)\
+            else contraction_mapping.get(match.lower())
+        expanded_contraction = first_char + expanded_contraction[1:]
+        return expanded_contraction
+    expanded_sentence = contractions_pattern.sub(expand_match, sentence)
+    return expanded_sentence
+
+
+if __name__ == '__main__':
+    # sent tokenize
+    sentences = nltk.sent_tokenize(corpus)
+
+    # test remove spec. char before full tokenize
+    clean_sents = [remove_characters_before_tokenization(sent, keep_apostrophes=True) for sent in sentences]
+
+    # test expand_contractions
+    expanded_sentences = [expand_contractions(sent, CONTRACTION_MAP) for sent in clean_sents]
+
+    print("After: {}".format(expanded_sentences))
