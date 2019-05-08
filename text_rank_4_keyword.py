cdc91a060c3e68593aad030f9e3adbe7b96ee762
Mason Edmison <mzbaran@uwm.edu>
2019-05-08 11:07:05 -0500

    formatting

--- text_rank_4_keyword.py
+++ text_rank_4_keyword.py
@@ -5,22 +5,6 @@ from spacy.lang.en.stop_words import STOP_WORDS
 
 nlp = spacy.load('en_core_web_sm')
 
-toy_text = """
-Elephants are large mammals of the family Elephantidae 
-and the order Proboscidea. Two species are traditionally recognised, 
-the African elephant and the Asian elephant. Elephants are scattered 
-throughout sub-Saharan Africa, South Asia, and Southeast Asia. Male 
-African elephants are the largest extant terrestrial animals. All 
-elephants have a long trunk used for many purposes, 
-particularly breathing, lifting water and grasping objects. Their 
-incisors grow into tusks, which can serve as weapons and as tools 
-for moving objects and digging. Elephants' large ear flaps help 
-to control their body temperature. Their pillar-like legs can 
-carry their great weight. African elephants have larger ears 
-and concave backs while Asian elephants have smaller ears 
-and convex or level backs.  
-"""
-
 
 class TextRank4Keyword():
     """Extract keywords from text"""
